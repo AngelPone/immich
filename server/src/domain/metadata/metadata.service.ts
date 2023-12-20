506bcd05dc6cf5420fae949388097385693414d2
Zack Pollard <zackpollard@ymail.com>
2023-12-20 01:23:11 +0000

    feat: auto-stack burst photos

--- server/src/domain/metadata/metadata.service.ts
+++ server/src/domain/metadata/metadata.service.ts
@@ -467,6 +467,7 @@ export class MetadataService {
       latitude: validate(tags.GPSLatitude),
       lensModel: tags.LensModel ?? null,
       livePhotoCID: (tags.ContentIdentifier || tags.MediaGroupUUID) ?? null,
+      autoStackId: this.getAutoStackId(tags),
       longitude: validate(tags.GPSLongitude),
       make: tags.Make ?? null,
       model: tags.Model ?? null,
@@ -484,6 +485,13 @@ export class MetadataService {
     }
 
     return { exifData, tags };
+  }
+
+  private getAutoStackId(tags: ImmichTags | null): string | null {
+    if (!tags) {
+      return null;
+    }
+    return tags.BurstID ?? tags.BurstUUID ?? tags.CameraBurstID ?? tags.MediaUniqueID ?? null;
   }
 
   private getDateTimeOriginal(tags: ImmichTags | Tags | null) {
